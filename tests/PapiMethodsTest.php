eb484bafd1f4bef1f7ca71216b7ad24ea6d98558
Jarrod Parkes <parkesfjarrod@gmail.com>
2021-08-03 11:42:11 -0500

    add phpunit

---
+++ tests/PapiMethodsTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class PapiMethodsTest extends TestCase
+{
+    public function testBasic()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
