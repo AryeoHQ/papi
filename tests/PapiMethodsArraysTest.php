1a7a8d888944e740c13aeaa220ed9d607cc4601d
Jarrod Parkes <parkesfjarrod@gmail.com>
2021-08-03 13:15:06 -0500

    test all papi methods for arrays

--- tests/PapiMethodsArraysTest.php
+++ tests/PapiMethodsArraysTest.php
@@ -15,8 +15,75 @@ class PapiMethodsArraysTest extends TestCase
         $this->papi_dir = getcwd();
     }
 
-    public function testValidFilePath()
+    public function testArrayFindRecursive()
     {
-        $this->assertTrue(true);
+        $array = [
+            'description' => 'top-level',
+            'obj' => [
+                'description' => 'mid-level',
+                'obj' => [
+                    'description' => 'low-level'
+                ]
+            ]
+        ];
+                
+        foreach (PapiMethods::arrayFindRecursive($array, 'description') as $index => $result) {
+            if ($index == 0) {
+                $this->assertEquals(
+                    $result,
+                    [
+                        'path' => '[description]',
+                        'value' => 'top-level'
+                    ]
+                );
+            } elseif ($index == 1) {
+                $this->assertEquals(
+                    $result,
+                    [
+                        'path' => '[obj][description]',
+                        'value' => 'mid-level'
+                    ]
+                );
+            } elseif ($index == 2) {
+                $this->assertEquals(
+                    $result,
+                    [
+                        'path' => '[obj][obj][description]',
+                        'value' => 'low-level'
+                    ]
+                );
+            }
+        }
+    }
+
+    public function testArrayKeysRecursive()
+    {
+        $array = [
+            'description' => 'top-level',
+            'obj' => [
+                'description' => 'mid-level',
+                'obj' => [
+                    'description' => 'low-level'
+                ]
+            ]
+        ];
+
+        $result = PapiMethods::arrayKeysRecursive($array);
+
+        $this->assertCount(5, $result);
+        
+        $this->assertCount(
+            3,
+            array_filter($result, function ($item) {
+                return $item == 'description';
+            })
+        );
+
+        $this->assertCount(
+            2,
+            array_filter($result, function ($item) {
+                return $item == 'obj';
+            })
+        );
     }
 }
