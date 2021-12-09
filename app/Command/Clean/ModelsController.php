9b693216cec05f467f3afaec895a62a57188b4d6
Jarrod Parkes <parkesfjarrod@gmail.com>
2021-12-09 15:26:48 -0600

    fix the clean to remove x-nullable since it isnt used in oas 3+

--- app/Command/Clean/ModelsController.php
+++ app/Command/Clean/ModelsController.php
@@ -16,14 +16,11 @@ class ModelsController extends PapiController
             ['format', 'spec format, defaults to JSON (JSON|YAML)', 'JSON', false],
             ['m_dir', 'models directory', '/examples/models', true],
         ];
-        $this->flags = [
-            ['oas_3_1_plus', 'clean models for inclusion in OAS specs that are v3.1+'],
-            ['override', 'apply known defaults if key/value pairs already exist']
-        ];
+        $this->flags = [];
         $this->notes = [
-            'Cleaning models applies known defaults for missing key/value pairs.',
-            'For example, adding `nullable|x-nullable` if not previously defined based on ',
-            'a model\'s required properties.',
+            'Cleaning models applies known defaults for specs.',
+            'For example, if `x-nullable` is declared (an old OAS 2 extension) then it ',
+            'removed.',
         ];
     }
 
@@ -53,38 +50,23 @@ class ModelsController extends PapiController
     public function cleanModel($model_file_path)
     {
         $array = PapiMethods::readSpecFile($model_file_path);
-        $override = $this->hasFlag('override');
 
         if ($array['properties']) {
-            $required_properties = [];
-
-            if (isset($array['required'])) {
-                $required_properties = $array['required'];
-            }
-
-            if (!$this->hasFlag('oas_3_1_plus')) {
-                $types_to_check = ['string', 'number', 'integer', 'boolean', 'array'];
-                foreach ($array['properties'] as $property_name => $property) {
-                    if (isset($property['type'])) {
-                        if (in_array($property['type'], $types_to_check, true)) {
-                            if (!isset($property['nullable']) || $override) {
-                                $property['nullable'] = !in_array($property_name, $required_properties, true);
-                            }
-                            if (!isset($property['x-nullable']) || $override) {
-                                $property['x-nullable'] = !in_array($property_name, $required_properties, true);
-                            }
+            $types_to_check = ['string', 'number', 'integer', 'boolean', 'array'];
+            foreach ($array['properties'] as $property_name => $property) {
+                if (isset($property['type'])) {
+                    if (in_array($property['type'], $types_to_check, true)) {
+                        if (isset($property['x-nullable'])) {
+                            unset($property['x-nullable']);
                         }
                     }
-                    if (isset($property['$ref'])) {
-                        if (!isset($property['nullable']) || $override) {
-                            $property['nullable'] = !in_array($property_name, $required_properties, true);
-                        }
-                        if (!isset($property['x-nullable']) || $override) {
-                            $property['x-nullable'] = !in_array($property_name, $required_properties, true);
-                        }
-                    }
-                    $array['properties'][$property_name] = $property;
                 }
+                if (isset($property['$ref'])) {
+                    if (isset($property['x-nullable'])) {
+                        unset($property['x-nullable']);
+                    }
+                }
+                $array['properties'][$property_name] = $property;
             }
         }
 
